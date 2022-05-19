9af8bbfe159cddd77df270acedbace5af75dc5b4
LukaszJarocki <93542313+LukaszJarocki@users.noreply.github.com>
2022-05-19 07:59:24 +0200

    added addVode function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  value: number = 0;
   showForm = false; //dzięki temu wysuwana klasa jest schowana
   quotes: Quotation[] = QUOTES;//pobranie listy do klasy komponentu, do listy quotation dopisujemy QUOTES z pliku database
-
+  quotation: Quotation = {author: '', sentence: '', votes: 0};
   onSwitchForm():void{
     this.showForm = !this.showForm //za każdym wywyołaniem odwraca wartość
   }
+
+  addQuotation(){
+    this.quotes.unshift(this.quotation)
+    this.quotation = {author: '', sentence: '', votes: 0};
+  }
+
+  addVote(quotation: Quotation, value: number){
+		quotation.votes += value;
+}
 }
